750d3f884fc3f6ac3da8f634e110a31932cabaa2
Ngo Quoc Vinh <vinhnq@hybrid-technologies.vn>
2025-05-19 10:12:28 +0700

    update

--- index.php
+++ index.php
@@ -14,7 +14,13 @@ echo "Hello World!";
 
 
 
-
+foreach ($argv as $arg) {
+    echo $arg . "\n";
+}
+echo "This is a test script.\n";
+echo "This script is used to demonstrate the use of PHP in a web application.\n";
+echo "This script is used to demonstrate the use of PHP in a web application.\n";
+echo "This script is used to demonstrate the use of PHP in a web application.\n";
 
 
 
